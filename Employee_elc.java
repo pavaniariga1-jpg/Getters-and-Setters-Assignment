19bf468ae88242b3fb0650901a6ae59214c1b805
Pavani Ariga <pavaniariga1@gmail.com>
2026-02-13 10:52:41 +0530

    Create Employee_elc.java

---
+++ Employee_elc.java
@@ -0,0 +1,27 @@
+package package_example;
+
+public class Employees {
+		public static void main(String[] args) {
+			 Employee e1= new Employee();
+			 e1.setEmployeeId(101);
+			 e1.setEmployeeName("pavani");
+			 e1.setMonthlySalary(30000);
+			 Employee e2=new Employee();
+			 e2.setEmployeeId(102);
+			 e2.setEmployeeName("hema");
+			 e2.setMonthlySalary(40000);
+			 Employee e3=new Employee();
+			 e3.setEmployeeId(103);
+			 e3.setEmployeeName("divya");
+			 e3.setMonthlySalary(50000);
+			 Employee e4=new Employee();
+			 e4.setEmployeeId(104);
+			 e4.setEmployeeName("mahesh");
+			 e4.setMonthlySalary(40000);
+			 Employee e5=new Employee();
+			 e5.setEmployeeId(105);
+			 e5.setEmployeeName("praneeth");
+			 e5.setMonthlySalary(60000);
+			 e1.displaydetails(); e2.displaydetails();
+		}
+	}
