b123cd964ce1553900317abe667de1780a0b7209
Pavani Ariga <pavaniariga1@gmail.com>
2026-04-06 18:57:22 +0530

    Create example2.java

---
+++ example2.java
@@ -0,0 +1,12 @@
+package package_example;
+import java.util.Scanner;
+public class Example2 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the value of a:");
+		int a=sc.nextInt();
+		System.out.println("Enter the value of b:");
+		int b=sc.nextInt();
+		System.out.println("additin of "+a+" and "+b+" is: "+(a+b));
+	}
+}
