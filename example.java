45e8f7136e865b7329bff2af0e4487bedd436fac
Pavani Ariga <pavaniariga1@gmail.com>
2026-04-06 18:56:34 +0530

    Create example.java

---
+++ example.java
@@ -0,0 +1,14 @@
+package package_example;
+import static java.lang.Math.*;
+import java.util.Scanner;
+public class Example1 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the value of a:");
+		int a=sc.nextInt();
+		System.out.println(sqrt(a));
+		System.out.println(PI);
+		
+	}
+
+}
